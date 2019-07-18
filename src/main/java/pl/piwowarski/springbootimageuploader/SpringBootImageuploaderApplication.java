39958e378bc643ab439fc5e30e2e1cc805f882d4
LukaszP <lukaszp4@onet.eu>
2019-07-19 01:29:54 +0200

    Initial commit

---
+++ src/main/java/pl/piwowarski/springbootimageuploader/SpringBootImageuploaderApplication.java
@@ -0,0 +1,13 @@
+package pl.piwowarski.springbootimageuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImageuploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/piwowarski/springbootimageuploader/SpringBootImageuploaderApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.piwowarski.springbootimageuploader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootImageuploaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
